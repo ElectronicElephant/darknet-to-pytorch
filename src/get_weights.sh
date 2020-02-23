60c7ed71106e517c754fa90056309965a97c589a
3t <1489272013@qq.com>
2020-02-23 15:51:55 +0800

    Script for downloading the weights

---
+++ src/get_weights.sh
@@ -0,0 +1,14 @@
+# Cleaning
+rm -rf *.weights *.cfg
+
+# YOLOv3
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget https://pjreddie.com/media/files/yolov3.weights
+
+# YOLOv3-spp
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-spp.cfg
+wget https://pjreddie.com/media/files/yolov3-spp.weights
+
+# YOLOv3-tiny
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.cfg
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
